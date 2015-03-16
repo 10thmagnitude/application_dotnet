a9019a7ade525da4f7e4e4b2fa8bea8feb37553f
Matt Stratton <matt.stratton@gmail.com>
2015-03-16 12:00:36 -0500

    Add contact info and license to metdata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'application_dotnet'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
-license          'all_rights'
-description      'Installs/Configures application_dotnet'
-long_description 'Installs/Configures application_dotnet'
+maintainer       '10th Magnitude'
+maintainer_email 'info@10thmagnitude.com'
+license          'Apache 2.0'
+description      'Deploys and configures .NET based applications'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
