a17e9aad8383a996c598c5911e5f76e9d468236b
Matt Stratton <matt.stratton@gmail.com>
2015-06-09 16:28:46 -0500

    Create fixture cookbook scaffold

---
+++ test/fixtures/cookbooks/webapp_test/metadata.rb
@@ -0,0 +1,7 @@
+name 'webapp_test'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures webapp_test'
+long_description 'Installs/Configures webapp_test'
+version '0.1.0'
